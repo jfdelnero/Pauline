e4e69739ad6c7d55b6204c24d7d86e1e06e533f9
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2020-08-25 15:16:02 +0000

    v1.1.1.2
    git-svn-id: svn://svn.code.sf.net/p/hxcfloppyemu/code/HxCFloppyEmulator/Pauline/trunk@2201 63fa2f70-6a9b-4bc4-b855-9c6a949b1d69

--- Softwares_Pauline/Pauline_control/inc/version.h
+++ Softwares_Pauline/Pauline_control/inc/version.h
@@ -1,5 +1,5 @@
-#define FILE_VERSION        1,1,1,1
-#define STR_FILE_VERSION   "1,1,1,1\0"
-#define STR_FILE_VERSION2  "1.1.1.1"
+#define FILE_VERSION        1,1,1,2
+#define STR_FILE_VERSION   "1,1,1,2\0"
+#define STR_FILE_VERSION2  "1.1.1.2"
 #define STR_DATE           "25 August 2020"
 
