6fea7a8cffb086b4487239eda30ad2d24d9f507a
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2023-08-28 08:25:15 +0200

    get the kernel image.

--- Linux_Pauline/targets/Pauline_RevA_de10-nano/config/kernel_post_process.sh
+++ Linux_Pauline/targets/Pauline_RevA_de10-nano/config/kernel_post_process.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
 # Cross compiler and Linux generation scripts
-# (c)2014-2019 Jean-François DEL NERO
+# (c)2014-2023 Jean-François DEL NERO
 #
 # DE10-Nano target kernel compilation
 # post process
 #
 
 source ${TARGET_CONFIG}/config.sh || exit 1
+
+cp ./arch/arm/boot/zImage ${TARGET_HOME}/output_objects/  || exit 1
