76feea503dd7425e54f32d682f81e54c8796a71c
Jean-François DEL NERO <jeanfrancoisdelnero@free.fr>
2023-08-28 08:53:14 +0200

    get the u-boot image.

---
+++ Linux_Pauline/targets/Pauline_RevA_de10-nano/config/uboot_post_process.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Cross compiler and Linux generation scripts
+# (c)2014-2019 Jean-François DEL NERO
+#
+# DE10-Nano uboot
+#
+
+source ${TARGET_CONFIG}/config.sh || exit 1
+
+cp ./u-boot.img ${TARGET_HOME}/output_objects/  || exit 1
